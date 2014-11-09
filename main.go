c7d8a427df877a6e86f20901b1df83c00552c126
Alex Kotliarskyi <alex.frantic@gmail.com>
2014-11-09 10:40:18 -0800

    Dummy log over async XHR

--- main.go
+++ main.go
@@ -6,13 +6,20 @@ import (
 	"net/http"
 )
 
-func logHandler(w http.ResponseWriter, r *http.Request) {
+func frontendHandler(w http.ResponseWriter, r *http.Request) {
 	ui, _ := ioutil.ReadFile("log.html")
+	w.Header().Set("Content-Type", "text/html")
 	w.Write(ui)
 }
 
+func logHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"log": ["a0372ba", "93caff1"]}`))
+}
+
 func main() {
-	fmt.Println("Visit http://localhost:8080/log")
+	fmt.Println("Visit http://localhost:8080/")
+	http.HandleFunc("/", frontendHandler)
 	http.HandleFunc("/log", logHandler)
 	http.ListenAndServe(":8080", nil)
 }
