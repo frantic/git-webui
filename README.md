f38becfc0005889caf0bc66ae0df891060141b13
Alex Kotliarskyi <alex.frantic@gmail.com>
2014-11-08 22:22:37 -0800

    Read dummy UI from html file

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ git-webui
 
 WARNING: Complete dogscience. I have no idea what I'm doing.
 
-The goal of the project is to creat modern, fast and easy to use tool for working with local git repositories. Potentially it will also have private sharing feature over local network (think hackathon projects).
+The goal of the project is to create a modern, fast and easy to use tool for working with local git repositories. Potentially it will also have private sharing feature over local network (think hackathon projects).
--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func logHandler(w http.ResponseWriter, r *http.Request) {
+	ui, _ := ioutil.ReadFile("log.html")
+	w.Write(ui)
 }
 
 func main() {
-	fmt.Println("Visit http://localhost:8080/git")
-	http.HandleFunc("/", handler)
+	fmt.Println("Visit http://localhost:8080/log")
+	http.HandleFunc("/log", logHandler)
 	http.ListenAndServe(":8080", nil)
 }
