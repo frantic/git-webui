9a901f24f3208efec9af6565a3417b2975ecb2cc
Alex Kotliarskyi <alex.frantic@gmail.com>
2014-11-13 09:50:29 -0800

    Figured out how to get diff

--- main.go
+++ main.go
@@ -20,6 +20,41 @@ type Commit struct {
 	Message string `json:"message"`
 }
 
+func getDiff() {
+	path, _ := os.Getwd()
+	repo, _ := git.OpenRepository(path)
+	walk, _ := repo.Walk()
+	var newTree, oldTree *git.Tree
+	walk.PushHead()
+	walk.Iterate(func(commit *git.Commit) bool {
+		if newTree == nil {
+			newTree, _ = commit.Tree()
+			return true
+		}
+		oldTree, _ = commit.Tree()
+		return false
+	})
+	opts := &git.DiffOptions{}
+	diff, err := repo.DiffTreeToTree(oldTree, newTree, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	diff.ForEach(func(delta git.DiffDelta, i float64) (git.DiffForEachHunkCallback, error) {
+		fmt.Println(delta.OldFile.Path, " -> ", delta.NewFile.Path)
+		return func(hunk git.DiffHunk) (git.DiffForEachLineCallback, error) {
+			fmt.Print(hunk.Header)
+			return func(line git.DiffLine) error {
+				if line.Origin == git.DiffLineAddition {
+					fmt.Print("+", line.Content)
+				} else {
+					fmt.Print("-", line.Content)
+				}
+				return nil
+			}, nil
+		}, nil
+	}, git.DiffDetailLines)
+}
+
 func logHandler(w http.ResponseWriter, r *http.Request) {
 	path, _ := os.Getwd()
 	repo, err := git.OpenRepository(path)
@@ -39,7 +74,6 @@ func logHandler(w http.ResponseWriter, r *http.Request) {
 			Message: commit.Message(),
 		}
 		commits = append(commits, *info)
-		fmt.Print(info)
 		return true
 	})
 
@@ -53,5 +87,6 @@ func main() {
 	fmt.Println("Visit http://localhost:8080/")
 	http.HandleFunc("/", frontendHandler)
 	http.HandleFunc("/log", logHandler)
-	http.ListenAndServe(":8080", nil)
+	// http.ListenAndServe(":8080", nil)
+	getDiff()
 }
